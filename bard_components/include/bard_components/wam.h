2686efdd7279d46abd99631d9d22668d53b4d4b3
Jonathan Bohren <jonathan.bohren+hg@gmail.com>
2012-03-07 10:35:44 -0500

    vector rosparams

--- bard_components/include/bard_components/wam.h
+++ bard_components/include/bard_components/wam.h
@@ -29,27 +29,29 @@ namespace bard_components {
 
   public:
     WAM(string const& name) :
-      TaskContext(name, RTT::base::TaskCore::PreOperational),
+      TaskContext(name, RTT::base::TaskCore::PreOperational)
       // Properties
-      n_wam_dof_(0),
-      can_dev_name_(""),
-      robot_model_xml_(""),
-      joint_prefix_(""),
+      ,n_wam_dof_(0)
+      ,can_dev_name_("")
+      ,robot_model_xml_("")
+      ,joint_prefix_("")
+      ,initial_positions_(7,0.0)
       // Throttle joint state publisher
-      joint_state_throttle_max_(10),
-      joint_state_throttle_counter_(0),
+      ,joint_state_throttle_max_(10)
+      ,joint_state_throttle_counter_(0)
       // Internal variables
-      canbus_(NULL),
-      robot_(NULL),
-      torques_(),
-      positions_(),
-      joint_state_()
+      ,canbus_(NULL)
+      ,robot_(NULL)
+      ,torques_()
+      ,positions_()
+      ,joint_state_()
     {
       // Declare properties (configuration variables)
-      this->addProperty("n_wam_dof",n_wam_dof_).doc("The name of the RTCAN device to which this WAM robot is connected.");
+      this->addProperty("n_wam_dof",n_wam_dof_).doc("The number of degrees-of-freedom of the WAM robot (4 or 7).");
       this->addProperty("can_dev_name",can_dev_name_).doc("The name of the RTCAN device to which this WAM robot is connected.");
       this->addProperty("robot_model_xml",robot_model_xml_).doc("The WAM URDF xml string.");
       this->addProperty("joint_prefix",joint_prefix_).doc("The joint name prefix used in the WAM URDF.");
+      this->addProperty("initial_positions",initial_positions_).doc("The calibration position of the robot.");
 
       // Configure data ports
       this->ports()->addEventPort("torques_in", torques_in_port_).doc("Input Event port: nx1 vector of joint torques. (n joints)");
@@ -79,18 +81,19 @@ namespace bard_components {
     }
 
     void calibrate_position(std::vector<double> &actual_positions) {
-      if(this->isRunning()) {
+      // Make sure we have a connection to the robot
+      if(this->isConfigured()) {
+        // Assign the positions to the current robot configuration
         if(robot_->SetPositions(Eigen::Map<Eigen::VectorXd>(&actual_positions[0], actual_positions.size()))
             != barrett_direct::WAM::ESUCCESS)
         {
-          std::cerr<<"Failed to set encoders!"<<std::endl;
+          std::cerr<<"Failed to calibrate encoders!"<<std::endl;
         }
         std::cerr<<"Calibrated encoders."<<std::endl;
       } else {
         std::cerr<<"Cannot calibrate encoders! The connection to the WAM robot on device "<<can_dev_name_<<" is not open."<<std::endl;
       }
     }
-
 
     bool configureHook() {
       // TODO: load URDF from  rosparam
@@ -123,19 +126,6 @@ namespace bard_components {
       // Prepare ports for realtime processing
       positions_out_port_.setDataSample(positions_);
       joint_state_out_port_.setDataSample(joint_state_);
-
-      return true;
-    }
-
-    bool startHook() {
-      // Check the data ports
-      if ( !torques_in_port_.connected() ) {
-        std::cerr<<"ERROR: No connection to \"torques_in\" for WAM on \""<<can_dev_name_<<"\"!"<<std::endl;
-        return false;
-      }
-      if ( !positions_out_port_.connected() ) {
-        std::cerr<<"WARNING: No connection to \"positions_out\" for WAM on \""<<can_dev_name_<<"\"!"<<std::endl;
-      }
       
       // Try to connect and initialize hardware
       try{
@@ -157,20 +147,34 @@ namespace bard_components {
           throw std::exception();
         }
 
-        Eigen::VectorXd q_init(n_wam_dof_);
-        q_init.setConstant( 0.0 );
-        q_init[1] = -M_PI_2;
-        q_init[3] =  M_PI;
+        // Set the joints to the calibration position
+        this->calibrate_position(initial_positions_);
 
-        // Set the calibration position
-        if( robot_->SetPositions( q_init ) != barrett_direct::WAM::ESUCCESS ){
-          std::cerr << "Failed to set position: " << q_init << std::endl;
-          return -1;
-        }
       } catch(std::exception &ex) {
         // Free the device handles
         this->cleanup_lowlevel();
         return false;
+      }
+
+      std::cout << "WAM connected on CAN device \""<<can_dev_name_<<"\"!" <<std::endl;
+
+      return true;
+    }
+
+    bool startHook() {
+      // Check the data ports
+      if ( !torques_in_port_.connected() ) {
+        std::cerr<<"ERROR: No connection to \"torques_in\" for WAM on \""<<can_dev_name_<<"\"!"<<std::endl;
+        return false;
+      }
+
+      if ( !positions_out_port_.connected() ) {
+        std::cerr<<"WARNING: No connection to \"positions_out\" for WAM on \""<<can_dev_name_<<"\"!"<<std::endl;
+      }
+      
+      // Set the robot to Activated
+      if( robot_->SetMode(barrett_direct::WAM::MODE_ACTIVATED) != barrett_direct::WAM::ESUCCESS ){
+        std::cerr<<"Failed to ACTIVATE WAM Robot on CAN device \""<<can_dev_name_<<"\""<<std::endl;
       }
 
       std::cout << "WAM started on CAN device \""<<can_dev_name_<<"\"!" <<std::endl;
@@ -207,9 +211,12 @@ namespace bard_components {
 
     void stopHook() {
       // Set the robot to IDLE
-      if( robot_->SetMode(0) != barrett_direct::WAM::ESUCCESS ){
+      if( robot_->SetMode(barrett_direct::WAM::MODE_IDLE) != barrett_direct::WAM::ESUCCESS ){
         std::cerr<<"Failed to IDLE WAM Robot on CAN device \""<<can_dev_name_<<"\""<<std::endl;
       }
+    }
+
+    void cleanupHook() {
       // Close the CANBus
       if( canbus_->Close() != leoCAN::CANBus::ESUCCESS ){
         std::cerr<<"Failed to close CAN device \""<<can_dev_name_<<"\""<<std::endl;
@@ -217,9 +224,6 @@ namespace bard_components {
 
       // Free the device handles
       this->cleanup_lowlevel();
-    }
-
-    void cleanupHook() {
     }
 
   private:
@@ -240,6 +244,7 @@ namespace bard_components {
     std::string can_dev_name_;
     std::string robot_model_xml_;
     std::string joint_prefix_;
+    std::vector<double> initial_positions_;
     size_t joint_state_throttle_max_;
     size_t joint_state_throttle_counter_;
 
