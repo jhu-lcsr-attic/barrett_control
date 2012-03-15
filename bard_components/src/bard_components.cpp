4ca4932cd9da535eaf639754c0346ded7cad47ac
Jonathan Bohren <jonathan.bohren+hg@gmail.com>
2012-03-15 02:45:28 -0400

    updating script for controller mux

--- bard_components/src/bard_components.cpp
+++ bard_components/src/bard_components.cpp
@@ -6,5 +6,6 @@
 
 ORO_CREATE_COMPONENT_LIBRARY()
 ORO_LIST_COMPONENT_TYPE(bard_components::WAM)
+ORO_LIST_COMPONENT_TYPE(bard_components::ControllerMux)
 ORO_LIST_COMPONENT_TYPE(bard_components::controllers::Trivial)
 ORO_LIST_COMPONENT_TYPE(bard_components::controllers::GravityCompensation)
