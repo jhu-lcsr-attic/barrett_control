a1df93a6c1a0a2334fb9c51e4980c86d2e092dfb
Jonathan Bohren <jonathan.bohren+hg@gmail.com>
2012-03-15 04:55:01 -0400

    adding rtt_bard_msgs, cleaning up launchfile params

---
+++ rtt_bard_msgs/include/bard_msgs/typekit/Types.hpp
@@ -0,0 +1,12 @@
+/* Generated from rtt_rosnode/src/Types.hpp.in */
+
+#ifndef __OROCOS_ROS_GENERATED_bard_msgs_TYPES_HPP
+#define __OROCOS_ROS_GENERATED_bard_msgs_TYPES_HPP
+
+// includes Types.hpp headers for all messages:
+#include "MuxState.h"
+#include "ControllerConfig.h"
+
+
+#endif
+
