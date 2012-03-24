1d443b5b876b2ea4ad4d3c14a19de9487ecbd4b9
Jonathan Bohren <jonathan.bohren+hg@gmail.com>
2012-03-24 02:53:03 -0400

    actually building the joint trajectory component into the lib

--- bard_components/src/bard_components.cpp
+++ bard_components/src/bard_components.cpp
@@ -7,6 +7,7 @@
 #include <bard_components/controllers/joint_pid.h>
 #include <bard_components/controllers/wrench.h>
 #include <bard_components/controllers/pose.h>
+#include <bard_components/controllers/joint_trajectory.h>
 
 ORO_CREATE_COMPONENT_LIBRARY()
 ORO_LIST_COMPONENT_TYPE(bard_components::WAM)
@@ -16,3 +17,4 @@ ORO_LIST_COMPONENT_TYPE(bard_components::controllers::GravityCompensation)
 ORO_LIST_COMPONENT_TYPE(bard_components::controllers::JointPID)
 ORO_LIST_COMPONENT_TYPE(bard_components::controllers::CartesianWrench)
 ORO_LIST_COMPONENT_TYPE(bard_components::controllers::CartesianPose)
+ORO_LIST_COMPONENT_TYPE(bard_components::controllers::JointTrajectory)
